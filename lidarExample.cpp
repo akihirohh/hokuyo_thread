b05d1fcfed67565d6acb3e5c7941929a4fa4c2be
vitorakihiro <akihirohh@gmail.com>
2017-08-15 10:21:03 -0500

    intensity added

--- lidarExample.cpp
+++ lidarExample.cpp
@@ -18,7 +18,7 @@ const std::string currentTime(){
 	struct tm	tstruct;
 	char		buf[80];
 	tstruct = *localtime(&now);
-	strftime(buf,sizeof(buf), "%Y%m%d|%H%M%S|", &tstruct);
+	strftime(buf,sizeof(buf), "%Y%m%d,%H%M%S,", &tstruct);
 	return buf;
 }
 int millis(timeval t_start)
@@ -31,12 +31,13 @@ int millis(timeval t_start)
 int main(int argc, char *argv[])
 {
 	//DATA_LOGGING
-	std::string lidar_filename;
-	std::fstream lidar_file;
+	std::string lidar_filename, intensity_filename;
+	std::fstream lidar_file, intensity_file;
 
 	//LIDAR
 	int previous_lidar_ts = 0, current_ts = 0;
-  std::vector<long> lidar_readings (1080);
+  std::vector<long> lidar_distance (1080);
+  std::vector<unsigned short> lidar_intensity(1080);
 	pthread_t thLidar;
 	lidarRead::thdata th_lidar;
 	th_lidar.b_loop = 0;
@@ -48,8 +49,11 @@ int main(int argc, char *argv[])
   struct timeval t_start;
 
   lidar_filename.append(currentDateTime());
+  intensity_filename = lidar_filename;
   lidar_filename.append("_lidar_ts.txt");
+  intensity_filename.append("_intensity_ts.txt");
   lidar_file.open(lidar_filename.c_str(), std::ios_base::out);
+  intensity_file.open(intensity_filename.c_str(), std::ios_base::out);
 
   pthread_create (&thLidar, NULL, &lidarRead::lidarReading, &th_lidar);
   while (!th_lidar.b_loop);
@@ -62,7 +66,8 @@ int main(int argc, char *argv[])
     try
     {
       th_lidar.mtx.lock();
-      std::copy(th_lidar.d.begin(), th_lidar.d.end()-1, lidar_readings.begin());
+      std::copy(th_lidar.distance.begin(), th_lidar.distance.end()-1, lidar_distance.begin());
+      std::copy(th_lidar.intensity.begin(), th_lidar.intensity.end(), lidar_intensity.begin());
       current_ts = th_lidar.timestamp;
       th_lidar.mtx.unlock();
     }
@@ -71,17 +76,23 @@ int main(int argc, char *argv[])
     if(current_ts != previous_lidar_ts)
     {
       std::cout << std::endl << timestamp << "\nLidar samples:\n";
-      for (int i = 0; i < lidar_readings.size(); i += lidar_readings.size()/10)
+      for (int i = 0; i < lidar_distance.size(); i += lidar_distance.size()/10)
       {
-        std::cout << "[" << i << "]: " << lidar_readings[i] << "\t";
+        std::cout << "[" << i << "]: " << lidar_distance[i] << " | "  << lidar_intensity[i] << "\t";;
       }
       previous_lidar_ts = current_ts;
       lidar_file << timestamp;
-      for (int i = 0; i < lidar_readings.size(); i++) //last one is lidar_timestamp
-        lidar_file << "|" << lidar_readings[i];
+      for (int i = 0; i < lidar_distance.size(); i++) //last one is lidar_timestamp
+        lidar_file << "," << lidar_distance[i];
       lidar_file << std::endl;
+
+      intensity_file << timestamp;
+      for (int i = 0; i < lidar_distance.size(); i++) //last one is lidar_timestamp
+        intensity_file << "," << lidar_intensity[i];
+      intensity_file << std::endl;
     }    
   }
   lidar_file.close();
+  intensity_file.close();
   std::cout << lidar_filename << std::endl; 
 }
